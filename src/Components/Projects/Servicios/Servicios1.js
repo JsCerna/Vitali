c55376b81ef0a258da1b4070782b1bea55e52d49
JsCerna <jcernat@udd.cl>
2023-08-03 00:43:28 -0400

    arreglo pagination final

--- src/Components/Projects/Servicios/Servicios1.js
+++ src/Components/Projects/Servicios/Servicios1.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Pagination, Button } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import SCard from "./ServiciosCard";
 import cardData from "./Cardata";
